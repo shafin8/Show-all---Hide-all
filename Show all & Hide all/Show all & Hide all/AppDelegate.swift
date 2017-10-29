b79b6c6b67858a3ddb0ec40d161303a86c727680
shafin <oshafin@gmail.com>
2017-10-30 01:44:10 +0600

    shows and hides all running apps in mac
    basic application.

---
+++ Show all & Hide all/Show all & Hide all/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Show all & Hide all
+//
+//  Created by shafin haq on 10/30/17.
+//  Copyright © 2017 shafin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Show all & Hide all/Show all & Hide all/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Show all & Hide all
+//
+//  Created by shafin haq on 10/30/17.
+//  Copyright © 2017 shafin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
